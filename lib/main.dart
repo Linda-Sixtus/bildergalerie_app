2dc5058f5485ec52a1d2fa82f4d77ed732f5eb0e
Linda Sixtus <linda.sixtus@gmx.de>
2025-07-22 16:02:21 +0200

    Aufgabe P15 Galery

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'galery.dart';
+import 'postview.dart';
+import 'user_info.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bildergalerie',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 93, 180, 124)),
+      ),
+      home: const MyHomePage(title: 'My Gallery'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int currentIndex = 0;
+
+  List<Widget> p15screens = [GaleryPage(), UserInfoPage()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: IndexedStack(index: currentIndex, children: p15screens),
+      ),
+       bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.image), label: "Bilder"),
+            NavigationDestination(icon: Icon(Icons.person), label: "Über mich"),
+          ],
+          selectedIndex: currentIndex,
+          onDestinationSelected: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+    );
+  }
+}
---
+++ lib/postview.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PostViewPage extends StatelessWidget {
+
+  const PostViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
